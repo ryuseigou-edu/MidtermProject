503420a91865017816c406d7dc332191288f5f5c
RyuseiNakaso <241101@std.hi-joho.ac.jp>
2025-06-20 14:12:53 +0900

    Characterの冗長けしたよ！

--- src/Character.java
+++ src/Character.java
@@ -6,7 +6,6 @@ public abstract class Character implements Creature {
         this.name = name;
         this.hp = hp;
     }
-    public abstract void attack(Character target);
 
     public final boolean isAlive(){
         return hp > 0;
