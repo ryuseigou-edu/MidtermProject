8d14fbe8b994471be36871ce5cae18bd3a1f2645
RyuseiNakaso <241101@std.hi-joho.ac.jp>
2025-06-20 11:48:24 +0900

    Heroクラスまでの追加

---
+++ src/Character.java
@@ -0,0 +1,17 @@
+public abstract class Character implements Creature {
+    String name;
+    int hp;
+
+    Character(String name, int hp){
+        this.name = name;
+        this.hp = hp;
+    }
+    public abstract void attack(Character target);
+
+    public final boolean isAlive(){
+        return hp > 0;
+    }
+    public void showStatus(){
+        System.out.println("["+name+"]：HP["+hp+"]");
+    }
+}
---
+++ src/Hero.java
@@ -0,0 +1,18 @@
+public class Hero extends Character {
+    String weapon;
+    Hero(String name, int hp, String weapon){
+        super(name, hp);
+        this.weapon = weapon;
+    }
+    @Override
+    public void attack(Character target){
+        System.out.println
+                (this.name + "は" + weapon + "で攻撃！" + target.name + "に10のダメージを与えた！");
+        target.hp -= 10;
+    }
+    public void heal(){
+        System.out.println
+                (name + "は回復呪文を唱えた！HPが20回復した！");
+        this.hp += 20;
+    }
+}
